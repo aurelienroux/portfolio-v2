c873e5c7c7585acf3ce19181af4d57299511852f
Aurélien Roux <aureroux13@gmail.com>
2020-12-19 12:56:01 -0500

    title & jobs components

---
+++ components/Jobs.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <li>2019-now - @Aerial</li>
+    <li>2018-2019 - @Clever Age NA</li>
+    <li>2017-2018 - @Adfab</li>
+    <li>2016-2017 - Freelance dev</li>
+  </ul>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/Title.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Aurelien Roux</h1>
+    <h2>front end & aspiring fullstack developer</h2>
+    <p>
+      I specialize in Vue, Nuxt and am currently learning Node & Express.
+      <br />
+      I believe in clean code,
+      <a
+        href="https://en.wikipedia.org/wiki/KISS_principle"
+        target="_blank"
+        rel="noopener noreferrer"
+        >KISS</a
+      >,
+      <a
+        href="https://en.wikipedia.org/wiki/You_aren%27t_gonna_need_it"
+        target="_blank"
+        rel="noopener noreferrer"
+        >YAGNI</a
+      >
+      and
+      <a
+        href="https://en.wikipedia.org/wiki/Convention_over_configuration"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        convention over configuration
+      </a>
+      principles
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped></style>
